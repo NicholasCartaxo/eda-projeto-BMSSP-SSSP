b49ebd250aa43b23329f9c776f79283156169e57
juliacruzandrade <julia.cruz.de.araujo.andrade@ccc.ufcg.edu.br>
2025-09-02 10:25:12 -0300

    added NodeDist

---
+++ src/main/java/commom/dataStructures/NodeDist.java
@@ -0,0 +1,44 @@
+package main.java.commom.dataStructures;
+
+import main.java.commom.graph.Node;
+
+public class NodeDist implements Comparable<NodeDist> {
+    public final Node node;
+    public final int dist;
+
+    public NodeDist(Node node, int dist){
+        this.node = node;
+        this.dist = dist;
+    }
+
+    @Override
+    public int hashCode() {
+        return node.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NodeDist other = (NodeDist) obj;
+        if (node == null) {
+            if (other.node != null)
+                return false;
+        } else if (!node.equals(other.node))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(NodeDist o) {
+        if(dist == o.dist)
+            return node.compareTo(o.node);
+        return Integer.valueOf(dist).compareTo(Integer.valueOf(o.dist));
+    }
+
+    
+}
--- src/main/java/commom/graph/Node.java
+++ src/main/java/commom/graph/Node.java
@@ -3,7 +3,7 @@ package main.java.commom.graph;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Node {
+public class Node implements Comparable<Node> {
     
     public final int id;
     public List<Edge> outEdges;
@@ -15,6 +15,30 @@ public class Node {
 
     public void addEdge(Edge e){
         outEdges.add(e);
+    }
+
+    @Override
+    public int compareTo(Node n2) {
+        return Integer.valueOf(id).compareTo(Integer.valueOf(n2.id));
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Node other = (Node) obj;
+        if (id != other.id)
+            return false;
+        return true;
     }
 
 }
